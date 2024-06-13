fa8edd33945a8382b2dcb44445e3b217d5e7c002
El Houssain Souhail <sou.ductive@gmail.com>
2024-06-13 23:33:02 +0100

    Added POST method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,9 @@
 """Flask App Module
 """
 from flask import Flask, jsonify
+from auth import Auth
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
@@ -10,6 +12,19 @@ def index() -> str:
     """Returns a jsonified welcome message
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users() -> str:
+    """POST method for users
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
