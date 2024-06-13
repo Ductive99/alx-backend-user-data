e8d1a36d5736d375fa038964a77b38bea9e17991
El Houssain Souhail <sou.ductive@gmail.com>
2024-06-13 19:08:15 +0100

    Added Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,28 @@
 """Authentication Module
 """
 import bcrypt
+from db import DB
+from user import User
+from typing import Union
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> Union[None, User]:
+        """Registers a new user with a hashed password
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        else:
+            raise ValueError(f"User {email} already exists")
 
 
 def _hash_password(password: str) -> bytes:
