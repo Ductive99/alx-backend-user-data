849a3028196a2742e85dcf274dd2deaedd824c57
El Houssain Souhail <sou.ductive@gmail.com>
2024-06-14 19:06:31 +0100

    Added Auth.destroy_session method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -60,11 +60,12 @@ class Auth:
             return None
         return user
 
-    def destroy_session(user_id: int) -> None:
+    def destroy_session(self, user_id: int) -> None:
         """Updates the corresponding user's session ID to None
         """
-        # TO-DO
-        pass
+        if not user_id:
+            return None
+        self._db.update_user(user_id, session_id=None)
 
 
 def _hash_password(password: str) -> bytes:
