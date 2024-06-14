9f802334089e460b8c6d7de7bf3eded31981bd1b
El Houssain Souhail <sou.ductive@gmail.com>
2024-06-14 20:32:46 +0100

    Added Auth.get_reset_password_token & get_reset_password_token function

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -67,6 +67,17 @@ class Auth:
             return None
         self._db.update_user(user_id, session_id=None)
 
+    def get_reset_password_token(self, email: str) -> str:
+        """Generates a token for the user with the given email
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except Exception:
+            raise ValueError
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
+
 
 def _hash_password(password: str) -> bytes:
     """Returns a hashed version of the given password
