09d2dc24abe516f4d211dc9e530dccb56fd22b82
El Houssain Souhail <sou.ductive@gmail.com>
2024-06-16 15:33:04 +0100

    Added 3 BasicAuth methods

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,8 @@ That defines the BasicAuth class
 """
 from .auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -33,3 +35,35 @@ class BasicAuth(Auth):
                 return result.decode('utf-8')
             except Exception:
                 return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str
+            ) -> (str, str):
+        """Returns the user email and password from the Base64 decoded value
+        """
+        if type(decoded_base64_authorization_header) == str:
+            if ":" not in decoded_base64_authorization_header:
+                return None
+            else:
+                data = decoded_base64_authorization_header.split(":", 1)
+                return (data[0], data[1])
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str
+            ) -> TypeVar('User'):
+        if type(user_email) == type(user_pwd) == str:
+            try:
+                users = User.search({'email': user_email})
+                if users[0].is_valid_password(user_pwd):
+                    return users[0]
+            except Exception:
+                return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieves user's data
+        """
+        auth_header = self.authorization_header(request)
+        b64_token = self.extract_base64_authorization_header(auth_header)
+        token = self.decode_base64_authorization_header(b64_token)
+        email, password = self.extract_user_credentials(token)
+        return self.user_object_from_credentials(email, password)
