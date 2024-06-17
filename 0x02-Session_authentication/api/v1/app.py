ed1f6df56389f812395a0b5bc98c0660eb872f94
El Houssain Souhail <sou.ductive@gmail.com>
2024-06-17 17:34:31 +0100

    Added missing comma

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -55,7 +55,7 @@ def authenticate():
         ep = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
             '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, ep):
